711e229e4c74b7915bfeb8db50db4cb327b8a18d
sai tarun Maryala <73954665+saitarun2002@users.noreply.github.com>
2022-11-28 00:39:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 
-var randomDiceimage = "images/" +  "dice" + randomNumber1 + ".png";
+var randomDiceimage =  "dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -8,7 +8,7 @@ image1.setAttribute("src", randomDiceimage);
 
 var randomNumber2 = Math.floor(Math.random()* 6) + 1;
 
-var randomimagesource2 = "images/dice" + randomNumber2 + ".png";
+var randomimagesource2 =  + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomimagesource2);
 
